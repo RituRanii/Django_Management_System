cd20dacdc077417502248f4634f8f0bfb77cc8d5
RituRanii <ritu199929@gmail.com>
2023-12-08 20:39:00 +0530

    course details

--- LMS/views.py
+++ LMS/views.py
@@ -4,8 +4,11 @@ def BASE(request):
     return render(request,'base.html')
 def HOME(request):
     category = Categories.objects.all().order_by('id')[0:5]
+    course = Course.objects.filter(status = 'PUBLISH').order_by('-id')
+
     context={
         'category':category,
+        'course':course,
     }
     return render(request,'Main/home.html',context)
 def SINGLE_COURSE(request):
