ddd43b27dee2974c4c85084b2db399bc9daba748
RituRanii <ritu199929@gmail.com>
2023-12-12 02:35:23 +0530

    Filter for course section and Search button update

--- LMS/views.py
+++ LMS/views.py
@@ -1,6 +1,9 @@
 
 from django.shortcuts import redirect,render
 from app.models import *
+from django.template.loader import render_to_string
+from django.http import JsonResponse
+
 def BASE(request):
     return render(request,'base.html')
 def HOME(request):
@@ -16,13 +19,45 @@ def SINGLE_COURSE(request):
     category = Categories.get_all_category(Categories)
     level = Level.objects.all()
     course = Course.objects.all()
+    FreeCourse_count = Course.objects.filter(price=0).count
+    PaidCourse_count = Course.objects.filter(price__gte=1).count()
     context = {
         'category':category,
         'level':level,
         'course':course,
+        'FreeCourse_count':FreeCourse_count,
+        'PaidCourse_count':PaidCourse_count,
     }
     return render(request,'Main/single_course.html',context)
+def filter_data(request):
+    category = request.GET.getlist('category[]')
+    level = request.GET.getlist('level[]')
+    price = request.GET.getlist('price[]')
+    if price == ['priceFree']:
+        course = Course.objects.filter(price=0)
+    elif price == ['pricePaid']:
+        course = Course.objects.filter(price__gte=1)
+    elif price == ['priceAll']:
+        course = Course.objects.all()
+    elif category:
+        course = Course.objects.filter(category__id__in=category).order_by('-id')
+    elif level:
+        course = Course.objects.filter(level__id__in=level).order_by('-id')
+    else:
+        course = Course.objects.all().order_by('-id')
+    context={
+        'course': course
+    }
+    t = render_to_string('ajax/course.html',context)
+    return JsonResponse({'data':t})
 def CONTACT_US(request):
     return render(request,'Main/contact_us.html')
 def ABOUT_US(request):
     return render(request,'Main/about_us.html')
+def SEARCH_COURSE(request):
+    query = request.GET['query']
+    course=Course.objects.filter(title__icontains = query)
+    context = {
+        'course': course,
+    }
+    return render(request,'search/search.html',context)
